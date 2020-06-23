29bcaccb7766818ff9e905397b609d699f00c74e
Bernát Kalló <kallo.bernat@gmail.com>
2020-06-23 10:06:51 +0200

    refactor: unify

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { UNIFY } from './unify'
+
 export type Clause<S> = S
 
 /**
@@ -18,34 +20,9 @@ export default class Eslog<Statement> {
   }
   * prove (goal: Statement) {
     for (const fact of this.clauses) {
-      for (const solution of this.unify(fact, goal)) {
+      for (const solution of (fact as any)[UNIFY](goal)) {
         yield solution
       }
-    }
-  }
-  * unify (a: any, b: any) {
-    if (a === b) {
-      yield
-    } else if (a instanceof Array && b instanceof Array) {
-      if (a.length === 0) {
-        if (b.length === 0) {
-          yield
-        } else {
-          // don't yield
-        }
-      } else {
-        if (b.length === 0) {
-          // don't yield
-        } else {
-          for (const binding of this.unify(a[0], b[0])) {
-            for (const binding2 of this.unify(a.slice(1), b.slice(1))) {
-              yield
-            }
-          }
-        }
-      }
-    } else {
-      // don't yield
     }
   }
 }
