fbf303345bb116328b1bd56d5fb8ac012a55731e
Bernát Kalló <kallo.bernat@gmail.com>
2020-06-26 15:52:08 +0200

    refactor: unification

--- src/term/unify-array.ts
+++ src/term/unify-array.ts
@@ -14,29 +14,29 @@ Array.prototype[UNIFY] = function * (that: unknown) {
   if (this.length === 0) {
     if (that.length === 0) yield
     else if (that[0] instanceof ArraySpread)
-      for (const _ of unify([], that[0].variable))
-        for (const _ of unify([], that.slice(1))) yield
+      for (const _ of that[0].variable[UNIFY](this))
+        for (const _ of that.slice(1)[UNIFY]([])) yield
   } else if (this[0] instanceof ArraySpread) {
     if (
       this.length === 1 &&
       that.length === 1 &&
       that[0] instanceof ArraySpread
     )
-      for (const _ of unify(this[0].variable, that[0].variable)) yield
+      for (const _ of this[0].variable[UNIFY](that[0].variable)) yield
     else if (this.length === 1)
-      for (const _ of unify(this[0].variable, that)) yield
+      for (const _ of this[0].variable[UNIFY](that)) yield
     else {
-      for (const _ of unify(this[0].variable, []))
-        for (const _ of unify(this.slice(1), that)) yield
+      for (const _ of this[0].variable[UNIFY]([]))
+        for (const _ of this.slice(1)[UNIFY](that)) yield
       const part = [createVariable(), new ArraySpread(createVariable())]
-      for (const _ of unify(this[0].variable, part))
-        for (const _ of unify([...part, ...this.slice(1)], that)) yield
+      for (const _ of this[0].variable[UNIFY](part))
+        for (const _ of [...part, ...this.slice(1)][UNIFY](that)) yield
     }
   } else {
     if (that.length === 0) {
       // no match
     } else if (that[0] instanceof ArraySpread) {
-      for (const _ of unify(that, this)) yield
+      for (const _ of that[UNIFY](this)) yield
     } else {
       for (const _ of unify(this[0], that[0]))
         for (const _ of this.slice(1)[UNIFY](that.slice(1))) yield
